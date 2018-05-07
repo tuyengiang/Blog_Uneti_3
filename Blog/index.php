fac4df16e489594857f4df9a18dbfc503e982355
Tuyển Giảng <nguyentuyengiangbn@gmail.com>
2018-05-07 22:58:48 +0700

    Blog uneti đơn giản

---
+++ Blog/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Blog cá nhân đơn giản</title>
+	<meta charset="utf-8"/>
+	<!--chen files css-->
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+
+<body>
+	  <div class="website">
+			<div class="header">
+				<a href="index.php"><h1>BlogUneti</h1></a>
+				<p>BLog cá nhân đơn giản</p>
+			</div><!--header-->
+
+			<div class="menu">
+				<ul>
+					<li><a href="index.php">Trang chủ</a></li>
+					<li><a href="#">Bài viết</a></li>
+					<li><a href="#">Đăng nhập</a></li>
+					<li><a href="#">Đăng ký</a></li>
+				</ul>
+			</div><!--menu-->
+
+			<div class="main">
+
+
+			</div><!--main-->
+
+			<div class="footer">
+
+			</div><!--footer-->
+
+	  </div><!--website-->
+</body>
+
+</html>
